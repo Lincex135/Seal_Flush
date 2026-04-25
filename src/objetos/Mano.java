cdb4616f7f719be87bc87de8559fa5a3cd6cfe3c
Ximena LR <lincex135@gmail.com>
2026-04-26 00:44:47 +0200

    FIX clases TipoMano y Mano

--- src/objetos/Mano.java
+++ src/objetos/Mano.java
@@ -9,10 +9,11 @@ public class Mano {
         cartas = new Carta[7];
         cartas[0] = cartasJugador[0];
         cartas[1] = cartasJugador[1];
-        cartas[3] = cartasTablero[0];
-        cartas[4] = cartasTablero[1];
-        cartas[5] = cartasTablero[2];
-        cartas[6] = cartasTablero[3];
+        cartas[2] = cartasTablero[0];
+        cartas[3] = cartasTablero[1];
+        cartas[4] = cartasTablero[2];
+        cartas[5] = cartasTablero[3];
+        cartas[6] = cartasTablero[4];
     }
 
     public Carta[] getCartas() {
--- src/util/TipoMano.java
+++ src/util/TipoMano.java
@@ -2,25 +2,35 @@ package util;
 
 // Los tipos de mano del poker, ordenados de menor a mayor valor
 public enum TipoMano {
+    CARTA_ALTA("Carta alta", 0),
+    PAREJA("Pareja", 1),
+    DOBLE_PAREJA("Doble pareja", 2),
+    TRIO("Trío", 3),
+    ESCALERA("Escalera", 4),
+    COLOR("Color", 5),
+    FULL_HOUSE("Full house", 6),
+    POKER("Póker", 7),
+    ESCALERA_DE_COLOR("Escalera de color", 8),
+    ESCALERA_REAL("Escalera real", 9);
 
-    CARTA_ALTA(0),
-    PAREJA(1),
-    DOBLE_PAREJA(2),
-    TRIO(3),
-    ESCALERA(4),
-    COLOR(5),
-    FULL_HOUSE(6),
-    POKER(7),
-    ESCALERA_DE_COLOR(8),
-    ESCALERA_REAL(9);
-
+    private final String descripcion;
     private final int valor;
 
-    TipoMano(int valor) {
+    TipoMano(String descripcion, int valor) {
+        this.descripcion = descripcion;
         this.valor = valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
     }
 
     public int getValue() {
         return valor;
+    }
+
+    @Override
+    public String toString() {
+        return descripcion;
     }
 }
